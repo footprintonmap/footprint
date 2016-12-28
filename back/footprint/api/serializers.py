435c87d5efee8adddaef91585d465d0d1bd2fc15
vv <vv@pc-141-167.customer.courtrooms.knightsbridge.lan>
2016-12-28 18:01:35 +0000

    upload image

--- back/footprint/api/serializers.py
+++ back/footprint/api/serializers.py
@@ -9,56 +9,7 @@ from rest_framework.serializers import (
 )
 from .models import *
 from django.db.models import Q
-from api.auth import get_token
-
-class Base64ImageField(ImageField):
-    """
-    A Django REST framework field for handling image-uploads through raw post data.
-    It uses base64 for encoding and decoding the contents of the file.
-
-    Heavily based on
-    https://github.com/tomchristie/django-rest-framework/pull/1268
-
-    Updated for Django REST framework 3.
-    """
-
-    def to_internal_value(self, data):
-        from django.core.files.base import ContentFile
-        import base64
-        import six
-        import uuid
-
-        # Check if this is a base64 string
-        if isinstance(data, six.string_types):
-            # Check if the base64 string is in the "data:" format
-            if 'data:' in data and ';base64,' in data:
-                # Break out the header from the base64 content
-                header, data = data.split(';base64,')
-
-            # Try to decode the file. Return validation error if it fails.
-            try:
-                decoded_file = base64.b64decode(data)
-            except TypeError:
-                self.fail('invalid_image')
-
-            # Generate file name:
-            file_name = str(uuid.uuid4())[:12] # 12 characters are more than enough.
-            # Get the file name extension:
-            file_extension = self.get_file_extension(file_name, decoded_file)
-
-            complete_file_name = "%s.%s" % (file_name, file_extension, )
-
-            data = ContentFile(decoded_file, name=complete_file_name)
-
-        return super(Base64ImageField, self).to_internal_value(data)
-
-    def get_file_extension(self, file_name, decoded_file):
-        import imghdr
-
-        extension = imghdr.what(file_name, decoded_file)
-        extension = "jpg" if extension == "jpeg" else extension
-
-        return extension
+from api.auth import get_token, Base64ImageField
 
 class UserSignUpSerializer(ModelSerializer):
     token = CharField(allow_blank=True, read_only=True)
@@ -139,7 +90,7 @@ class ImageSerializer(ModelSerializer):
     image = Base64ImageField(max_length=None, use_url=True)
     class Meta:
         model = Image
-        fields = ('name', 'description', 'image', 'album')
+        fields = ('id', 'name', 'description', 'image', 'album')
 
     def create(self, validated_data):
         name = validated_data['name']
